fa92a513e0cc62c17d1f5ca8cb17ad8cd1cec8c7
Enno Huang <hi.xiaolonghuang@gmail.com>
2021-01-01 20:21:48 +1300

    Added 206. Reverse Linked List

---
+++ src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode();
+    }
+
+    /**
+     * Assume that we have linked list 1 → 2 → 3 → Ø, we would like to change it to Ø ← 1 ← 2 ← 3.
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
